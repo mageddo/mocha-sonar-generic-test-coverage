a442ca03628469ced5d0ac710eb1cd95b621a64b
Elvis de Freitas <edigitalb@gmail.com>
2017-04-21 12:16:12 -0300

    #5 completing test

--- test/Gruntfile.js
+++ test/Gruntfile.js
@@ -9,12 +9,12 @@ module.exports = function(grunt) {
       test: {
         options: {
           reporter: 'mocha-sonar-generic-test-coverage',
-          captureFile: 'results.txt', // Optionally capture the reporter output to a file 
+          captureFile: 'out/results.txt', // Optionally capture the reporter output to a file 
           quiet: false, // Optionally suppress output to standard out (defaults to false) 
           clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false) 
           noFail: false // Optionally set to not fail on failed tests (will still fail on other errors) 
         },
-        src: ['test/**/*.js']
+        src: ['main/**/*.js']
       }
     }
   });
